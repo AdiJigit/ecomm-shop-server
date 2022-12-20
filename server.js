1a9c46eb896261a9fcb31d770bd66927a6717607
AdiJigit <adijigit@gmail.com>
2022-12-20 14:31:55 +0600

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 
 // it is used for the put or post method only, so that we can save what we are sending(object, req.body)
 app.use(cors({
-  origin: ['http://loacalhost:3000', 'https://jigit.onrender.com'],
+  origin: ['http://loacalhost:5000', 'https://jigit.onrender.com'],
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
